eb34d6b86482ca59bfccd6694ff6586d3ad2fc4f
Anuja <step4@Step4s-MacBook-Pro.local>
2019-11-21 14:11:02 +0530

    deleted unnecessary files for testing

--- runAllTests.sh
+++
@@ -1,6 +0,0 @@
-node test/testUtilLib.js 
-node test/testInputLib.js
-node test/testGetPattern.js
-node test/testRectanglePatterns.js
-node test/testTrianglePattern.js
-node test/testDiamondPatterns.js
